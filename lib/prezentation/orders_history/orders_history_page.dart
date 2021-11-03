bb1ece341159f2d95843a3a1e96af41f62f6babc
lizapolishchuk <kbpjxtr2301>
2021-11-03 13:51:59 +0200

    changed app name

---
+++ lib/prezentation/orders_history/orders_history_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/widgets.dart';
+
+class OrdersHistoryPage extends StatefulWidget {
+  const OrdersHistoryPage({Key? key}) : super(key: key);
+
+  @override
+  _OrdersHistoryPageState createState() => _OrdersHistoryPageState();
+}
+
+class _OrdersHistoryPageState extends State<OrdersHistoryPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
+}
